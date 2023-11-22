2abfb2baa415db68bf698e779cff991e930d0b3d
Aathesh Vigram <31738722+ash-gram@users.noreply.github.com>
2023-11-22 23:56:25 +0530

    code_correction
    Added functional code to solve the problem. Old bit was just some copy-pasta from C-GPT

--- Module 2/Lab 2/sumOfNumWithUnitDigitK.py
+++ Module 2/Lab 2/sumOfNumWithUnitDigitK.py
@@ -1,12 +1,34 @@
-def minSetSize(k,sum):
-    if sum == 0:
+def minimumNumbers(num: int, k: int) -> int:
+    if num==0:
         return 0
-    for i in range(1,10):
-        if (i*k)<=sum and (k*i)%10 == sum%10:
-            return i
-    return -1
-
-k = int(input())
-sum = int(input())
-
-print(minSetSize(k, sum))
+    if k==0:
+        if num%10==0:
+            return 1
+        return -1
+    if num<k:
+        return -1
+    d = {
+            1:[1,2,3,4,5,6,7,8,9,0],
+            2:[2,4,6,8,0],
+            3:[3,6,9,2,5,8,1,4,7,0],
+            4:[4,8,2,6,0],
+            5:[5,0],
+            6:[6,2,8,4,0],
+            7:[7,4,1,8,5,2,9,6,3,0],
+            8:[8,6,4,2,0],
+            9:[9,8,7,6,5,4,3,2,1,0]
+    }
+    if num%10 not in d[k]:
+        return -1
+    else:
+        i = d[k].index(num%10) + 1
+        if num<i*k:
+            return -1
+        return i
+try:
+    num = int(input())
+    k = int(input())
+    result = minimumNumbers(num, k)
+    print(result)
+except:
+    print('-1')
